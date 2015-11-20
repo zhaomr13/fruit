350821f4bf84e4603986ed04d45b2a44c9094a98
Mingrui Zhao <mingrui.zhao@cern.ch>
2015-11-21 07:14:19 +0800

    delete lib

--- lib/fruit/test_this_module.py
+++
@@ -1,8 +0,0 @@
-import utils
-print locals()
-print utils._prefix("hello/good")
-print utils._prefix("good")
-print utils._suffix("hello/good")
-print utils._suffix("good")
-
-a = 'hello my name is zhaomr'
